43e08f5527eca092705172eca6403b3d506ec35f
alidarozi <128852865+aldrzi@users.noreply.github.com>
2023-12-24 21:48:01 +0700

    ok

--- settings.tsx
+++ settings.tsx
@@ -2,8 +2,8 @@
 // label has to be the exact same as in your candy machine config
 export const mintText = [
   { label: "WL", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
-  { label: "TEAM", mintText: "Mint Paying with ABC", buttonLabel: "Mint now!", header: "TEAM Mint" },
-  { label: "Public", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" },
+  { label: "TEAM", mintText: "TEAM", buttonLabel: "Mint now!", header: "TEAM Mint" },
+  { label: "Public", mintText: "Get Your Magic Now!", buttonLabel: "Mint now!", header: "Public Mint" },
   { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
 ];
 
